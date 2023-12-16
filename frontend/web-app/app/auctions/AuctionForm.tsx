10c12cb3d0877fa24ec31ab4bdaaa0387f9909b3
navid asadipur <navid.asadipur@gmail.com>
2023-12-16 16:17:48 +0330

    add general input component. up to end of section 9.6

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -1,7 +1,37 @@
+'use client'
+
+import { Button, TextInput } from 'flowbite-react';
 import React from 'react'
+import { FieldValue, FieldValues, useForm } from 'react-hook-form'
+import Input from '../components/Input';
 
 export default function AuctionForm() {
+  const { register, handleSubmit, setFocus,
+    formState: { isSubmitted, isValid, isDirty, errors } } = useForm();
+
+  function onSubmit(data: FieldValues) {
+    console.log(data);
+  }
+
   return (
-    <div>Auiction form goes here</div>
+    <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
+      <Input label='Make' name='make' />
+      <div className='mb-3 block'>
+        <TextInput
+          {...register('model', { required: 'Model is required' })}
+          placeholder='Model'
+          color={errors?.model && 'failure'}
+          helperText={errors.model?.message as string}
+        />
+      </div>
+      <div className='flex justify-between'>
+        <Button outline color='gray'>Cancel</Button>
+        <Button 
+          isProcessing={isSubmitted} 
+          // disabled={!isValid}
+          type='submit'
+          outline color='success'>Submit</Button>
+      </div>
+    </form>
   )
 }
