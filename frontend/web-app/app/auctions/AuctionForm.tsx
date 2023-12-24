0c06be6ed7e52006d16b7931b8acd1d3283a5cfa
navid asadipur <navid.asadipur@gmail.com>
2023-12-24 20:02:45 +0330

    add fetchWrapper and make auction form work. end of section 9.22

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -5,10 +5,13 @@ import React, { useEffect } from 'react'
 import { FieldValue, FieldValues, useForm } from 'react-hook-form'
 import Input from '../components/Input';
 import DateInput from '../components/DateInput';
+import { createAuction } from '../actions/auctionActions';
+import { useRouter } from 'next/navigation';
 
 export default function AuctionForm() {
+  const router = useRouter();
   const { control, handleSubmit, setFocus,
-    formState: { isSubmitted, isValid, isDirty, errors } } = useForm({
+    formState: { isSubmitted, isValid } } = useForm({
       mode: 'onTouched'
     });
 
@@ -16,8 +19,16 @@ export default function AuctionForm() {
       setFocus('make');
     }, [setFocus])
 
-  function onSubmit(data: FieldValues) {
-    console.log(data);
+  async function onSubmit(data: FieldValues) {
+    try {
+      const res = await createAuction(data);
+      if (res.error) {
+        throw new Error(res.error);
+      }
+      router.push(`/auctions/details/${res.id}`);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -56,7 +67,7 @@ export default function AuctionForm() {
         <Button outline color='gray'>Cancel</Button>
         <Button
           isProcessing={isSubmitted}
-          // disabled={!isValid}
+          disabled={!isValid}
           type='submit'
           outline color='success'>Submit</Button>
       </div>
